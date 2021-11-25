b06272e9c3e7603b260ec451ad1b1b3d80a6e6f5
dxx <xud015@ucsd.edu>
2021-11-25 15:19:05 -0800

    feat(network): init of connected graph and add fuction to check users connection

--- entity/network.py
+++ entity/network.py
@@ -14,6 +14,9 @@ from .user import User
 import numpy as np
 import numpy.random as npr
 import numpy.matlib as matlib
+import Queue
+from random import randrange
+from config import Config
 from util.multithreading import ThreadPool
 
 
@@ -22,7 +25,7 @@ class Network:
     Network entity.
     '''
 
-    def __init__(self, rows, cols, users: list):
+    def __init__(self, rows=Config.network_row, cols=Config.network_col, users: list):
         '''
         Initialize the network.
 
@@ -37,13 +40,35 @@ class Network:
 
         # Initialize the network.
         self.network_matrix = matlib.empty((rows, cols), dtype=User)
+        pos = {}
+        for _, user in users:
+            pos_temp = [randrange(rows), randrange(cols)]
+            j = 0
+            while j != len(pos):
+                if(pos_temp == pos[j]):
+                    pos_temp = [randrange(rows), randrange(cols)]
+                else:
+                    j = j+1
+            pos[user.id] = pos_temp
+            self.network_matrix[pos_temp[0], pos_temp[1]] = users[i]
+
         # self.network_matrix[1,2] = self.users[1]
         # Initialize the connect matrix.
         # TODO: initalized as all True for testing. Should be all False.
-        self.connect_matrix = matlib.ones(
-            (len(users), len(users)), dtype=bool)
+        self.connect_matrix = matlib.zeros((len(users), len(users)), dtype=bool)
+        for _, user1 in users:
+            for _,user2 in users:
+                if user1 != user2:
+                    com_radius = min(user1.radius,user2.randius)
+                    vec1 = np.array(pos[user1])
+                    vec2 = np.array(pos[user2])
+                    distance = np.linalg.norm(vec1,vec2)
+                    if(distance <= com_radius):
+                        self.connect_matrix[user1.id,user2.id] = True
+                        self.connect_matrix[user2.id,user1.id] = True
+            
 
-        # Initialize the thread pool.
+            # Initialize the thread pool.
         self.thread_pool = ThreadPool(users)
 
         # TODO: randomly set users in the network.
@@ -59,11 +84,29 @@ class Network:
         Step 2: build the connect matrix.
         '''
 
-        def is_connected_graph(self):
+        def is_connected_graph():
             '''
             Check if the network is connected.
             '''
-            return True
+            #initialize checked user dict
+            user_checked = {}
+            for user_id in self.users:
+                user_checked[user_id] = False
+            #bfs determine graph is connect or not
+            q = Queue.Queue()
+            q.put(list(self.users.keys())[0])
+            user_checked[1] = True
+            count = 1
+            while not q.empty():
+                current_id = q.get()
+                connected_user = get_connected_users(self, current_id)
+                for i in range(len(connected_user)):
+                    if user_checked[connected_user[i]] == False:
+                        q.put(connected_user[i])
+                        count = count + 1
+
+
+            return count == len(self.users)
 
         pass  # TODO
         if not is_connected_graph(self):
@@ -76,7 +119,9 @@ class Network:
         # TODO: To be implemented. Currently, return all other users.
         connected_users_id_list = []
         for my_user_id in self.users:
-            if my_user_id != user_id:
+            # if my_user_id != user_id:
+            #     connected_users_id_list.append(my_user_id)
+            if(self.connect_matrix[user_id,my_user_id]):
                 connected_users_id_list.append(my_user_id)
 
         return connected_users_id_list
@@ -86,7 +131,7 @@ class Network:
         Check if two users are connected.
         '''
         # TODO: To be implemented. Currently, return True.
-        return True
+        return self.connect_matrix[sender.id,reciever.id]
 
     def _get_user_by_id(self, user_id):
         '''
